b6de2e12938c1fce43755c2974b470cea6984f2c
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-06-16 09:39:09 +0900

    feat: implement PNS class
    Add class header

--- src/skpns/__init__.py
+++ src/skpns/__init__.py
@@ -1,1 +1,17 @@
 """Principal nested spheres analysis."""
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+__all__ = [
+    "PNS",
+]
+
+
+class PNS(TransformerMixin, BaseEstimator):
+    """Principal nested spheres (PNS) analysis [1]_.
+
+    References
+    ----------
+    .. [1] Jung, Sungkyu, Ian L. Dryden, and James Stephen Marron.
+       "Analysis of principal nested spheres." Biometrika 99.3 (2012): 551-568.
+    """
