bcbb00015581fefcd4e7a205526df2bd3596ff4c
Jisoo Song <jeesoo9595@snu.ac.kr>
2025-12-22 12:33:32 +0900

    feat: add test for InverseExtrinsicPNS

--- tests/test_onnx.py
+++ tests/test_onnx.py
@@ -2,16 +2,16 @@ import numpy as np
 import onnxruntime as rt
 from skl2onnx import to_onnx
 
-from skpns import ExtrinsicPNS, IntrinsicPNS
+from skpns import ExtrinsicPNS, IntrinsicPNS, InverseExtrinsicPNS
 from skpns.util import circular_data
 
 
 def test_IntrinsicPNS_onnx(tmp_path):
     path = tmp_path / "pns.onnx"
 
+    pns = IntrinsicPNS()
     X = circular_data().astype(np.float32)
-    pns = IntrinsicPNS().fit(X)
-    Xpred = pns.transform(X)
+    Xpred = pns.fit_transform(X)
 
     onx = to_onnx(pns, X[:1])
     with open(path, "wb") as f:
@@ -28,11 +28,30 @@ def test_IntrinsicPNS_onnx(tmp_path):
 def test_ExtrinsicPNS_onnx(tmp_path):
     path = tmp_path / "pns.onnx"
 
+    pns = ExtrinsicPNS()
     X = circular_data().astype(np.float32)
-    pns = ExtrinsicPNS().fit(X)
-    Xpred = pns.transform(X)
+    Xpred = pns.fit_transform(X)
 
     onx = to_onnx(pns, X[:1])
+    with open(path, "wb") as f:
+        f.write(onx.SerializeToString())
+
+    sess = rt.InferenceSession(path, providers=["CPUExecutionProvider"])
+    input_name = sess.get_inputs()[0].name
+    label_name = sess.get_outputs()[0].name
+    Xpred_onnx = sess.run([label_name], {input_name: X})[0]
+
+    assert np.all(np.isclose(Xpred, Xpred_onnx, atol=1e-3))
+
+
+def test_InverseExtrinsicPNS_onnx(tmp_path):
+    path = tmp_path / "pns.onnx"
+
+    pns = ExtrinsicPNS()
+    X = pns.fit_transform(circular_data()).astype(np.float32)
+    Xpred = pns.inverse_transform(X)
+
+    onx = to_onnx(InverseExtrinsicPNS(pns), X[:1])
     with open(path, "wb") as f:
         f.write(onx.SerializeToString())
 
